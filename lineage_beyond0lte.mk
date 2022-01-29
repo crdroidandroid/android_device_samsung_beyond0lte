105c0a20d27582e3e840965eb58253899f9f841d
Orel <Orely4242@gmail.com>
2022-01-29 20:35:42 +0200

    beyond0lte: adapt to crDroid

--- lineage_beyond0lte.mk
+++ lineage_beyond0lte.mk
@@ -17,29 +17,28 @@
 ## Inherit from generic products, most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-## Product API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-
-## Inherit from beyond0lte device
-$(call inherit-product, device/samsung/beyond0lte/device.mk)
-
-## Boot Animation
-TARGET_BOOTANIMATION_HALF_RES := true
-TARGET_SCREEN_HEIGHT := 2280
-TARGET_SCREEN_WIDTH := 1080
-
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+## Inherit from beyond0lte device
+$(call inherit-product, device/samsung/beyond0lte/device.mk)
+
+## Inherit some common crDroid stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+## Boot Animation
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 3040
+TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1080
+
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_beyond0lte
 PRODUCT_DEVICE := beyond0lte
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-G970F
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy S10e
+PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
